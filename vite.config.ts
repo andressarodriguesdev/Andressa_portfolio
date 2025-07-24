d544bb52d7be8a189725e272512f32f1ba3c8d7f
Andressa <andressa.rodrigues.2172@gmail.com>
2025-07-24 00:51:32 -0300

    Ajuste no vit.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'index.html',
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-});
-
+  build:{
+    outDir: "build"
+  }
+})
