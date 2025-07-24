848989805441e7139f82f674a141bb46ef0f6097
Andressa <andressa.rodrigues.2172@gmail.com>
2025-07-24 00:54:55 -0300

    Ajuste

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-  build:{
-    outDir: "build"
-  }
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
+});
