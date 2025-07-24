e936dfdd7011d273e4d67a8b819f002e9e25f3cb
Andressa <andressa.rodrigues.2172@gmail.com>
2025-07-23 21:39:01 -0300

    Alterações

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // <== ADICIONADO: base relativa para produção
   server: {
     host: "::",
     port: 8080,
